60170c28618947b1fdb117ffaf8e13e2ef3799f9
Mahesh <maheshzalte2000@gmail.com>
2024-09-02 14:36:15 +0530

    Bug Fixed Geting Gallary Images in Object

--- app/route/Trainer/trainerProfile.js
+++ app/route/Trainer/trainerProfile.js
@@ -127,9 +127,11 @@ router.get("/:id", async (req, res) => {
     const gallarysWithoutImages = await gallary.find({
       trainer_id: { $in: trainerId },
     });
-    const gallarys = await gallarysWithoutImages[0]?.photos?.map((photo) =>
-      photo ? `${baseUrl}/${photo.replace(/\\/g, "/")}` : ""
-    );
+    const gallarys = await gallarysWithoutImages[0]?.photos?.map((photo) => {
+      return {
+        photos: photo ? `${baseUrl}/${photo.replace(/\\/g, "/")}` : "",
+      };
+    });
 
     const currentDate = new Date().toISOString();
 
